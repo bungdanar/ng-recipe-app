ab40b26f0cb63d6f7d80dda0b9658cd2ce4cd0d1
Bung Danar <danar.g.putera@gmail.com>
2023-11-22 11:42:32 +0700

    End of Section 3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Ramen',
+      'Spicy Ramen',
+      'https://images.pixexid.com/spicy-ramen-bowl-noodles-swirling-in-a-fiery-red-broth-adorned-with-toppings-mvldomj2.jpeg'
+    ),
+    new Recipe(
+      'Udon',
+      'Shrimp Udon',
+      'https://www.hwcmagazine.com/wp-content/uploads/2018/08/Shrimp-Tempura-Udon-Noodles-9.jpg'
+    ),
+  ];
 }
