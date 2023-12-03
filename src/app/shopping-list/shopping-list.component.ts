5c8018776256a975f72db64c444e7bb46fecde16
Bung Danar <danar.g.putera@gmail.com>
2023-12-03 20:15:04 +0700

    End of Section 14

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private serviceSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsUpdatedEvent.subscribe((value) => {
-      this.ingredients = value;
-    });
+
+    this.serviceSubscription =
+      this.shoppingListService.ingredientsUpdatedEvent.subscribe((value) => {
+        this.ingredients = value;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.serviceSubscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-  ingredientsUpdatedEvent = new EventEmitter<Ingredient[]>();
+  ingredientsUpdatedEvent = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -15,11 +15,11 @@ export class ShoppingListService {
 
   addIngredient(value: Ingredient) {
     this.ingredients.push(value);
-    this.ingredientsUpdatedEvent.emit(this.getIngredients());
+    this.ingredientsUpdatedEvent.next(this.getIngredients());
   }
 
   addIngredients(value: Ingredient[]) {
     this.ingredients.push(...value);
-    this.ingredientsUpdatedEvent.emit(this.getIngredients());
+    this.ingredientsUpdatedEvent.next(this.getIngredients());
   }
 }
